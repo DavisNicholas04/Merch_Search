e58004ad1e38d48f7774594d9e6ddb232598bc23
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-10-17 02:05:41 -0400

    moved controller functions to the controller dir

--- src/service/mal.api.service.go
+++ src/service/mal.api.service.go
@@ -1,13 +1,1 @@
 package service
-
-import (
-	"github.com/jamespearly/loggly"
-	"merchSearch/controller"
-)
-
-func RequestMalBytes(malClient *loggly.ClientType, endpoint string) []byte {
-	response, httpErr := controller.GetCompleted(endpoint, "Curiossity")
-	HttpErrorCheck(httpErr, malClient)
-
-	return GetBytes(response, malClient)
-}
