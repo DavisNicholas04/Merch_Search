0004c0a441b100f4d23faec74f57019325548476
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-10-24 01:48:25 -0400

    edited loggy msg
    status sends back method, ip, request uri, and status code

--- ndavis20_server/src/controller/controller.go
+++ ndavis20_server/src/controller/controller.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"ndavis20_server/handler"
+	"ndavis20_server/model"
+	"ndavis20_server/service"
 	"ndavis20_server/utils"
 	"net/http"
 	"os"
@@ -16,18 +18,26 @@ type Status struct {
 
 func GetStatus(writer http.ResponseWriter, request *http.Request) {
 	statusClient := utils.InstantiateClient("GET_status")
+	logglyModel := model.LogglyStatus{
+		Method:          request.Method,
+		SourceIpAddress: utils.GetOutboundIP().String(),
+		RequestPath:     request.RequestURI,
+		StatusCode:      -999,
+	}
+
 	switch request.Method {
 	case http.MethodGet:
-		writer.Header().Set("Content-Type", "application/json")
-		encoderErr := json.NewEncoder(writer).Encode(Status{SystemTime: time.Now()})
-		if encoderErr != nil {
-			clientErr := statusClient.EchoSend("error", fmt.Sprintf("%v", encoderErr))
-			handler.ClientErrorCheck(clientErr)
-		}
+		service.EncodeJson(writer, statusClient)
+		logglyModel.StatusCode = http.StatusOK
 	default:
 		http.Error(writer, "Method Not Allowed", http.StatusMethodNotAllowed)
+		logglyModel.StatusCode = http.StatusMethodNotAllowed
 	}
-	clientErr := statusClient.EchoSend("info", request.Method)
+
+	logglyMarshal, err := json.Marshal(logglyModel)
+	handler.MarshalError(err, statusClient, "logglyStatusModel")
+
+	clientErr := statusClient.EchoSend("info", string(logglyMarshal))
 	handler.ClientErrorCheck(clientErr)
 }
 
@@ -36,6 +46,18 @@ func PutEbayDeletionNotification(writer http.ResponseWriter, request *http.Reque
 	clientErr := statusClient.EchoSend("info", request.Method)
 	handler.ClientErrorCheck(clientErr)
 
+	// check if the ebay deletion notification is a put request and continue if so.
+	// else exit the function early and report StatusMethodNotAllowed.
+	switch request.Method {
+	case http.MethodPut:
+		break
+	default:
+		http.Error(writer, "Invalid verification token", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// check if the verification token sent in the header is the correct token. If so notify ebay with a 200 OK
+	// else report StatusUnauthorized
 	switch request.Header.Get("Authorization") {
 	case os.Getenv("VERIFICATION_TOKEN"):
 		fmt.Println(fmt.Sprintf("Method:%v\nHeader:%v\nBody:%v", request.Method, request.Header, request.Body))
