c12411cbcb41c3085ed9bfb26eb0fb6c1506011e
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-10-30 01:11:17 -0400

    added getstatus(), getItemCount, and checkLiveCountRegex

--- ndavis20_server/src/service/querry_param.service.go
+++ ndavis20_server/src/service/querry_param.service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"ndavis20_server/model"
+	"ndavis20_server/utils"
 	"net/http"
 	"os"
 	"regexp"
@@ -10,10 +12,7 @@ func CheckItemIdReg(itemId string, writer http.ResponseWriter) bool {
 	itemIdReg, _ := regexp.Compile("^((v[0-9])\\|([0-9]{1,12})\\|([0-9]{1,12}))$")
 	if !itemIdReg.MatchString(itemId) {
 		http.Error(
-			writer,
-			"Malformed request, check your itemId",
-			http.StatusBadRequest,
-		)
+			writer, "Malformed request, check your itemId", http.StatusBadRequest)
 		return false
 	}
 	return true
@@ -24,10 +23,7 @@ func CheckUserIdReg(userId string, writer http.ResponseWriter) bool {
 
 	if !userIdReg.MatchString(userId) {
 		http.Error(
-			writer,
-			"Malformed request, check your userId",
-			http.StatusBadRequest,
-		)
+			writer, "Malformed request, check your userId", http.StatusBadRequest)
 		return false
 	}
 	return true
@@ -46,4 +42,38 @@ func CheckTableRegex(table string, writer http.ResponseWriter) bool {
 		return false
 	}
 	return true
+}
+
+func CheckLiveCountRegex(liveCount string, writer http.ResponseWriter) bool {
+	liveCountReg, _ := regexp.Compile("^(true|false)$")
+	if !liveCountReg.MatchString(liveCount) && liveCount != "" {
+		http.Error(writer, "Malformed request, check your userId", http.StatusBadRequest)
+		return false
+	}
+	return true
+}
+
+func getItemCount(tableName string, liveCount string) int64 {
+	var itemCount int64
+	if liveCount == "true" {
+		itemCount = *utils.GetLiveItemCount(tableName)
+	} else {
+		itemCount = *utils.GetItemCount(tableName)
+	}
+	return itemCount
+}
+
+func GetStatus(liveCount string) model.Status {
+	tableName := os.Getenv("DYNAMO_DB_TABLE_NAME")
+
+	var status model.Status
+
+	itemCount := getItemCount(tableName, liveCount)
+
+	status = model.Status{
+		TableName:   tableName,
+		RecordCount: itemCount,
+	}
+
+	return status
 }
